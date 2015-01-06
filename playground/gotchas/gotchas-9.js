511683fa5a6e4f0ccbfb194994b5dab469aa2ac1
Pablo Rosales <me@mooddha.com>
2015-01-06 16:54:50 -0600

    Updating IIFE file on gotchas section.

--- playground/gotchas/gotchas-9.js
+++ playground/gotchas/gotchas-9.js
@@ -13,4 +13,61 @@ test('Javascript gotchas #9 (IIFE: immediately-invoked function expression)', fu
         // window.undefined = true;
     }());
 
+    (function() {
+
+        function bugged() {
+            var l = [];
+            for(var i=0; i < 10; i++) {
+                // i will be a reference to the variable i, and not its current value
+                l[i] = function() {
+                    return i;
+                }
+            }
+            return l;
+        }
+
+        var r = bugged();
+        strictEqual(r[0](), 10);
+        strictEqual(r[3](), 10);
+        strictEqual(r[9](), 10);
+
+        function notBugged() {
+            var l = [];
+            for(var i=0; i < 10; i++) {
+                (function() {
+                    // j will capture the current value of i in this IIFE
+                    var j = i;
+                    l[i] = function() {
+                        return j;
+                    }
+                }());
+            }
+            return l;
+        }
+
+        var r2 = notBugged();
+        strictEqual(r2[0](), 0);
+        strictEqual(r2[3](), 3);
+        strictEqual(r2[9](), 9);
+
+        function notBugged2() {
+            var l = [];
+            for(var i=0; i < 10; i++) {
+                // j is the current value of i in this IIFE
+                (function(j) {
+                    l[j] = function() {
+                        return j;
+                    }
+                }(i));
+            }
+            return l;
+        }
+
+        var r3 = notBugged2();
+        strictEqual(r3[0](), 0);
+        strictEqual(r3[3](), 3);
+        strictEqual(r3[9](), 9);
+
+    }());
+
 });
