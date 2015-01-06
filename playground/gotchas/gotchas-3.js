4a6d1baa77f18a76682b7f4ed63c76f3e57f908b
Pablo Rosales <me@mooddha.com>
2015-01-06 16:40:34 -0600

    Update to hoisting file on gotchas section.

--- playground/gotchas/gotchas-3.js
+++ playground/gotchas/gotchas-3.js
@@ -26,4 +26,36 @@ test('Javascript gotchas #2 (Hoisting)', function() {
 
     strictEqual(hoistedVar(), 0);
 
+    // Even tho there are 3 declarations of i, only one and not 3 are declared
+    // Is the equivalent to 'var x = 0, i;' at the first line of the function
+    // Notice that those declarations are turned into assignments so i will be undefined before the second line
+    // of the function
+    function repeatFoo() {
+        var x = 0;
+        for(var i=0; i < 10; i++) {
+            x++;
+        }
+        for(var i=0; i < 10; i++) {
+            x++;
+        }
+        for(var i=0; i < 10; i++) {
+            x++;
+        }
+        return x;
+    }
+
+    strictEqual(repeatFoo(), 30);
+
+    var foo = 'foo';
+
+    // this won't be hoisted, foo refers to the caught exception
+    try {
+        throw "exception"
+    }
+    catch (foo) {
+        foo = 'bar';
+    }
+
+    strictEqual(foo, 'foo');
+
 });
